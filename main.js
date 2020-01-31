c97b9f0c9618113a3aa8e24380bebe82e2f712ac
Akihito Mikazuki <jennasisis@gmail.com>
2020-01-30 20:01:21 -0500

    Update main.js
    A few changes here,
    * Whitespace to section out bits of code
    * env variables should always be UPPER_SNAKE_CASE

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 const fs = require("fs")
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+const prefix = process.env.PREFIX
 client.commands = new Discord.Collection();
-const prefix = process.env.prefix
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
@@ -30,4 +31,4 @@ client.on('message', message => {
 
 });
 
-client.login(process.env.token);
+client.login(process.env.TOKEN);
